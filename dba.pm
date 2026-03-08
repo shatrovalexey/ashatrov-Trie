346cfa0afab7e48e0e7a63b1f03850c6e71054e5
Алексей <mail@ashatrov.ru>
2026-03-08 17:01:37 +0300

    Добавил вывод времени обработки

--- dba.pm
+++ dba.pm
@@ -1,7 +1,7 @@
 use strict;
 use DBI;
 
-+ DBI->connect('dbi:mysql:test', 'test', 'test', {
++ DBI->connect('dbi:mysql:test', 'root', 'f2ox9erm', {
 	'RaiseError' => 1
 	, 'mysql_auto_reconnect' => 1
 	, 'mysql_use_result ' => 1
